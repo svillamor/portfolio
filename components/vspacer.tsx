709ad6571bf8097ffd0f616c92cb83461ae8a8ed
svillamor <samira.villamor@gmail.com>
2023-11-26 08:49:03 -0500

    spacer testing

--- components/vspacer.tsx
+++ components/vspacer.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 
 export const Vspacer = () => {
-	return (
-        <section>
-        {/* LINE SPACER - EMPTY */}
-                            <div className="flex flex-row mx-4 sm:mx-20 w-full">
-                            
-                                <div className="grid grid-cols-3 w-10 sm:w-12 place-items-center">
-                                    <div className=""></div>
-                                    <div className="w-px h-16 sm:h-24 bg-slate-300"></div>
-                                    <div className=""></div>
-                                </div>    
-                                <div className="">
-                                {/* EMPTY */ }
-                                </div>                        
-                                                        
-                            </div>
-            {/* END LINE SPACER - EMPTY*/} 
-        </section>    
-    );
-}
+  return (
+    <section>
+      {/* LINE SPACER - EMPTY */}
+      <div className="flex flex-row mx-4 sm:mx-20 w-full">
+        <div className="grid grid-cols-3 w-10 sm:w-12 place-items-center">
+          <div className=""></div>
+          <div className="w-px h-16 sm:h-24 bg-slate-300"></div>
+          <div className=""></div>
+        </div>
+        <div className="">{/* EMPTY */}</div>
+      </div>
+      {/* END LINE SPACER - EMPTY*/}
+    </section>
+  );
+};
